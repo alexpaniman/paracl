fb07a267392b97569e27c4e6e7f02fce932aa320
alexpaniman <alexpaniman@gmail.com>
2025-02-15 04:31:56 +0300

    wip: notes alignment + fix

--- src/lexer/driver.cpp
+++ src/lexer/driver.cpp
@@ -17,13 +17,15 @@ int main(int argc, const char *argv[]) {
     std::vector<paracl::token> tokens = paracl::tokenize(source.text);
     paracl::print_tokens(tokens);
 
-    for (uint32_t i = 0; i < tokens.size(); ++ i) {
+    for (uint32_t i = 0; i < tokens.size() - 2; ++ i) {
 
         paracl::display_tokens(source, paracl::message_type::NOTE, "printing an annotated range", {
+            { tokens[i],  paracl::CYAN + paracl::BOLD + "notes:" + paracl::RESET + " #i" },
+            { tokens[i+1],  paracl::CYAN + paracl::BOLD + "notes:" + paracl::RESET + " #i+1" },
+            { tokens[i+2],  paracl::CYAN + paracl::BOLD + "notes:" + paracl::RESET + " #i+2" },
             { tokens[5],  "" },
             { tokens[15], "" },
-            { tokens[4],  paracl::CYAN + paracl::BOLD + "notes:" + paracl::RESET + " opening «{»" },
-            { tokens[42],  paracl::CYAN + paracl::BOLD + "notes:" + paracl::RESET + " closing «}»" },
+            { tokens[4],  paracl::CYAN + paracl::BOLD + "notes:" + paracl::RESET + " #i" },
         });
 
         std::cout << "\n";
